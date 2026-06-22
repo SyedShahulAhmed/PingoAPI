5b63bd5013a46cf9cc481f3d65ca63ae4b88d530
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-06-22 13:57:18 +0530

    Add : User Signup page

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,12 @@
+import SignupForm from "@/components/auth/signup-form";
+
+
+export default function SignupPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center px-4">
+      <div className="w-full max-w-md">
+        <SignupForm />
+      </div>
+    </div>
+  );
+}
