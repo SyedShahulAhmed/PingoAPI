afe3708e94ec200eee48d3583aa937056fcfb824
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-06-23 03:20:48 +0530

    Fix : Remove Unnecessary console logs

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -3,32 +3,16 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = process.env.JWT_SECRET!;
 
 export function generateToken(userId: string) {
-    return jwt.sign(
-        { userId },
-        JWT_SECRET,
-        { expiresIn: "7d" }
-    );
+  return jwt.sign({ userId }, JWT_SECRET, { expiresIn: "7d" });
 }
 export function verifyToken(token: string) {
-    try {
-        console.log("JWT_SECRET:", JWT_SECRET);
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
 
-        const decoded = jwt.verify(
-            token,
-            JWT_SECRET
-        );
-
-        console.log("DECODED:", decoded);
-
-        return decoded as {
-            userId: string;
-        };
-    } catch (error) {
-        console.error(
-            "VERIFY TOKEN ERROR:",
-            error
-        );
-
-        return null;
-    }
+    return decoded as {
+      userId: string;
+    };
+  } catch {
+    return null;
+  }
 }
