c296e3b7cb3a5243389c8a35a77140c9d47f0278
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-06-30 22:17:26 +0530

    Add : Login and Signup Actions

---
+++ src/actions/auth/login.ts
@@ -0,0 +1,67 @@
+"use server";
+
+import bcrypt from "bcryptjs";
+
+import { cookies } from "next/headers";
+
+import { connectDB } from "@/lib/db";
+
+import User from "@/models/user.model";
+
+import { generateToken } from "@/lib/jwt";
+
+import { LoginInput, LoginSchema } from "@/validators/login-validator";
+
+export async function login(data: LoginInput) {
+  try {
+    await connectDB();
+
+    const validated = LoginSchema.safeParse(data);
+
+    if (!validated.success) {
+      return {
+        success: false,
+        message: validated.error.errors[0].message,
+      };
+    }
+
+    const { email, password } = validated.data;
+
+    const user = await User.findOne({
+      email,
+    });
+
+    if (!user) {
+      return {
+        success: false,
+        message: "Invalid credentials",
+      };
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return {
+        success: false,
+        message: "Invalid credentials",
+      };
+    }
+
+    const token = generateToken(user._id.toString());
+    (await cookies()).set("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      path: "/",
+    });
+
+    return {
+      success: true,
+    };
+  } catch {
+    return {
+      success: false,
+      message: "Something went wrong",
+    };
+  }
+}
---
+++ src/actions/auth/signup.ts
@@ -0,0 +1,54 @@
+"use server";
+
+import bcrypt from "bcryptjs";
+
+import { connectDB } from "@/lib/db";
+
+import User from "@/models/user.model";
+import { SignupInput, SignupSchema } from "@/validators/signup-validator";
+
+export async function signup(data: SignupInput) {
+  try {
+    await connectDB();
+
+    const validated = SignupSchema.safeParse(data);
+
+    if (!validated.success) {
+      return {
+        success: false,
+        message: validated.error.errors[0].message,
+      };
+    }
+
+    const { name, email, password } = validated.data;
+
+    const existingUser = await User.findOne({
+      email,
+    });
+
+    if (existingUser) {
+      return {
+        success: false,
+        message: "User already exists",
+      };
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return {
+      success: true,
+      message: "Account created successfully",
+    };
+  } catch {
+    return {
+      success: false,
+      message: "Something went wrong",
+    };
+  }
+}
